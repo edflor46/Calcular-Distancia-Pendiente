5f5a0e6e4d5543ed504748f8933549e0a047061a
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-06 13:16:35 -0600

    Calculo de la pendiente y distancia

--- src/com/edflor/Geoanalitic.java
+++ src/com/edflor/Geoanalitic.java
@@ -1,6 +1,107 @@
 package com.edflor;
 
+
 public class Geoanalitic {
+    /*
+     * VARIABLES PRIVATE
+     * Variables private para acceder mediante metodos GET y SET (Encapsulamiento)
+     * */
+    private Integer x1;
+    private Integer y1;
+    private Integer x2;
+    private Integer y2;
 
+    /*
+     * CONSTRUCTOR INICIALIZADO
+     * Constructor vacio para inicializar el objeto de la clase.
+     * */
+    public Geoanalitic() {
+    }
 
+    /*
+     * ENCAPSULAMIENTO DE DATOS PARA TENER ACCESO DESDE OTRA CLASE (GET & SET)
+     * Es una buena practica encapsular los datos cuando la clase sera invocada
+     * desde otra clase.
+     * */
+
+    public Integer getX1() {
+        return x1;
+    }
+
+    public void setX1(Integer x1) {
+        this.x1 = x1;
+    }
+
+    public Integer getY1() {
+        return y1;
+    }
+
+    public void setY1(Integer y1) {
+        this.y1 = y1;
+    }
+
+    public Integer getX2() {
+        return x2;
+    }
+
+    public void setX2(Integer x2) {
+        this.x2 = x2;
+    }
+
+    public Integer getY2() {
+        return y2;
+    }
+
+    public void setY2(Integer y2) {
+        this.y2 = y2;
+    }
+
+    public Double distancia(Integer x1, Integer y1, Integer x2, Integer y2) {
+        /*
+         * Calculo de la distancia utilizando la clase Math
+         * */
+        Double calculo = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("La distancia entre los puntos (x1: " + x1 + ", y1: " + y1 + ", x2: " + x2 + ", y2: " + y2 + ") es: " + calculo);
+
+        /*
+         * Retorno de los datos
+         * */
+        return calculo;
+    }
+
+    public Double pendiente(Integer x1, Integer y1, Integer x2, Integer y2) {
+        /*
+         * Inicializacion de la variable calculo
+         * */
+        int calculo = 0;
+
+        /*
+         * Trycatch
+         * */
+        try {
+            /*
+             * Si los argumentos de las abcisas no son iguales se ejecuta el codigo
+             * */
+            calculo = (y2 - y1) / (x2 - x1);
+            System.out.println("La distancia entre los puntos (x1: " + x1 + ", y1: " + y1 + ", x2: " + x2 + ", y2: " + y2 + ") es: " + calculo);
+
+        }
+        /*
+         * Si los argumentos de las abcisas son iguales se ejecuta la exepcion para evitar la division entre 0
+         * */ catch (ArithmeticException e) {
+            System.out.println("Las abcisas de los puntos son iguales y tienden a infinito");
+            System.out.println();
+        }
+
+        /*
+         * Retorno del calculo
+         * Se castea a double en caso de que el resultado tenga decimales
+         * */
+        return (double) calculo;
+    }
+
+    public void help(){
+        System.out.println("Algo importante...");
+        System.out.println();
+    }
 }
--- src/com/edflor/Main.java
+++ src/com/edflor/Main.java
@@ -1,8 +1,123 @@
 package com.edflor;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /* MENU
+         * Menu para ejecutar las funciones Distancia y Pendiente.
+         * Variable sc para solicitar los datos desde la consola
+         * Variable opcion para ingresar a las funciones Distancia y Pendiente
+         * */
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        System.out.println("***== Bienvenid@, selecciona alguna opcion ==***");
+        System.out.println("1.- Calculo de Distancia");
+        System.out.println("2.- Calculo de la Pendiente");
+        System.out.println("3.- Help");
+        System.out.println("0.- Salir");
+
+        /* DO WHILE
+         * El bucle se ejecutara mientras la opcion ingresada
+         * sea diferente de 0
+         * */
+        do {
+
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    invocarDistancia();
+                    System.out.println("presione 1 para volver a calcular la distancia");
+                    System.out.println("presione 2 para calcular la pendiente");
+                    System.out.println("3.- Help");
+                    System.out.println("Presione 0 para salir");
+                    break;
+                case 2:
+                    invocarPendiente();
+                    System.out.println("1.- calcular la distancia");
+                    System.out.println("2.- volver a  calcular la pendiente");
+                    System.out.println("3.- Help");
+                    System.out.println("0.- salir");
+                    break;
+
+                case 3:
+                    invocarHelp();
+                    System.out.println("1.- Calculo de Distancia");
+                    System.out.println("2.- Calculo de la Pendiente");
+                    System.out.println("3.- Help");
+                    System.out.println("0.- Salir");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+                    System.out.println("1.- Calculo de Distancia");
+                    System.out.println("2.- Calculo de la Pendiente");
+                    System.out.println("3.- Help");
+                    System.out.println("0.- Salir");
+            }
+        } while (opcion != 0);
+        System.out.println("Adios...");
+    }
+
+    /*
+    * INVOCARDISTANCIA
+    * Instancia de la clase Geoanalitic para ejecutar sus metodos
+    * geoanalitic.distancia()
+    * */
+    private static void invocarDistancia() {
+        System.out.println("//====================** Calcular la distancia entre dos puntos de una recta **====================//");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese X1: ");
+        Integer x1 = sc.nextInt();
+
+        System.out.println("Ingrese y1: ");
+        Integer y1 = sc.nextInt();
+
+        System.out.println("Ingrese X2: ");
+        Integer x2 = sc.nextInt();
+
+        System.out.println("Ingrese y2: ");
+        Integer y2 = sc.nextInt();
+
+        Geoanalitic geoanalitic = new Geoanalitic();
+        geoanalitic.distancia(x1, y1, x2, y2);
+    }
+
+    /*
+     * INVOCARPENDIENTE
+     * Instancia de la clase Geoanalitic para ejecutar sus metodos
+     * geoanalitic.pendiente()
+     * */
+    private static void invocarPendiente() {
+        System.out.println("//====================** Calcular la pendiente entre dos puntos **====================//");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese X1: ");
+        Integer x1 = sc.nextInt();
+
+        System.out.println("Ingrese y1: ");
+        Integer y1 = sc.nextInt();
+
+        System.out.println("Ingrese X2: ");
+        Integer x2 = sc.nextInt();
+
+        System.out.println("Ingrese y2: ");
+        Integer y2 = sc.nextInt();
+
+        Geoanalitic geoanalitic = new Geoanalitic();
+        geoanalitic.pendiente(x1, y1, x2, y2);
+    }
+
+    /*
+     * INVOCARHELP
+     * Instancia de la clase Geoanalitic para ejecutar sus metodos
+     * geoanalitic.help()
+     * */
+    private static void invocarHelp(){
+        Geoanalitic geoanalitic = new Geoanalitic();
+        geoanalitic.help();
     }
 }
